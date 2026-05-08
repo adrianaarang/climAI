487cdc6fc103972f50815e26d02ec34ed0d30094
unknown <javier.carretero.romero@gmail.com>
2026-05-08 10:16:34 +0200

    feat(db): aggregate all models for Alembic schema discovery

---
+++ app/db/base.py
@@ -0,0 +1,14 @@
+from app.db.base_class import Base
+from app.models.database import (
+    Province,
+    Unit,
+    Level,
+    Station,
+    Record,
+    User,
+    Metric,
+    Threshold,
+    Alert,
+    RecordAlert,
+    SentAlert
+)
